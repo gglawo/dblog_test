28d7b9ca4421e5dad84a622d18014a9152830f98
168 <guocheng1987@163.com>
2014-10-30 18:13:32 +0800

    v 1.0.1

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+#from django.shortcuts import render
 
 # Create your views here.
+from blog.models import Article, Tag, Classification
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+            
+def blog_list(request):
+    blogs = Article.objects.all().order_by('-publish_time')
+    return render_to_response('index.html', {"blogs": blogs}, context_instance=RequestContext(request))
